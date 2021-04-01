cff6b7e8bbee9e5d1ad80961c2e09b57bbf3f819
Alexis Dietl <alexisdietl@gmail.com>
2021-04-01 17:01:12 -0300

    first commit

---
+++ linear.py
@@ -0,0 +1,18 @@
+def linear_searc(list,target):
+    for i in range(0, len(list)):
+        if list[i] == target:
+            return i
+    return None
+
+def verify(index):
+    if index is not None:
+        print("Target found at index: ", index)
+    else:
+        print("Target not found in list")
+
+numbers = [1,2,3,4,5,6,7,8,9,10]
+
+result = linear_searc(numbers, 12)
+verify(result)
+result = linear_searc(numbers, 6)
+verify(result)
